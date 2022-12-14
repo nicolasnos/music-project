35969d913182d38de98b741f6505c8a7f70a9de2
Nicolas Olmos <nicolas.nos@hotmail.com>
2022-12-14 13:29:11 -0500

    css details

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ let nombre = document.getElementById("nombre");
 let email = document.getElementById("email");
 let mensaje = document.getElementById("mensaje");
 
-function perro() {
+function click() {
   alert("tu mensaje se ha enviado");
   console.log(`tu nombre es ${nombre.value}`);
   console.log(`tu email es ${email.value}`);
   console.log(`lo que quieres decir es ${mensaje.value}`);
 }
 
-boton.addEventListener("click", perro);
+boton.addEventListener("click", click);
