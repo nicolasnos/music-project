4ea3e00deebdae9c7250de997de2bf0f170dd099
Nicolas Olmos <nicolas.nos@hotmail.com>
2023-03-24 19:25:12 -0500

    home arreglado

--- prueba.js
+++ prueba.js
@@ -1,13 +1,57 @@
-const formContact = document.querySelector(".contacto");
+const formContact = document.querySelector(".contacto__form");
 
-const email = document.getElementById("email");
-const nombre = document.getElementById("nombre");
-const mensaje = document.getElementById("mensaje");
-
-formContact.addEventListener("submit", function (event) {
+formContact.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log(email.value);
-  console.log(nombre.value);
-  console.log(mensaje.value);
-  alert("tu mensaje fue enviado");
+
+  // Elimina los errores de los que ya pasam
+  document
+    .querySelectorAll(".error")
+    ?.forEach((item) => item.classList.remove("error"));
+
+  // convierte los valores de los imputs en un objeto
+  const data = Object.fromEntries(new FormData(event.target));
+  const { correo, mensaje, nombre } = data;
+
+  // Expresiones regulares
+  const emailValidation = new RegExp(
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  );
+  const nameValidator = new RegExp(
+    /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$/
+  );
+
+  // Condicionales para los datos
+  if (emailValidation.test(correo) && correo.length > 7) {
+    if (nameValidator.test(nombre) && nombre.length > 10) {
+      if (mensaje.length > 30) {
+        localStorage.setItem(
+          "dataUserV1",
+          JSON.stringify({
+            correo,
+            mensaje,
+            nombre,
+          })
+        );
+      } else {
+        document.querySelector('input[name="mensaje"]').classList.add("error");
+      }
+    } else {
+      document.querySelector('input[name="nombre"]').classList.add("error");
+    }
+  } else {
+    document.querySelector('input[name="correo"]').classList.add("error");
+  }
+});
+
+const imprimir = document.querySelector("#getUsers");
+
+imprimir.addEventListener("click", function (event) {
+  const data = JSON.parse(localStorage.getItem("dataUserV1"));
+  console.log(data);
+
+  document.querySelector("#dataUserGet").innerHTML += `
+    <li>${data.nombre}</li>
+    <li>${data.correo}</li>
+    <li>${data.mensaje}</li>
+  `;
 });
